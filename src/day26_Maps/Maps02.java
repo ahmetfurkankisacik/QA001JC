ede007b83da516c1162089a534ef8e3853793273
Ahmet Furkan KISACIK <ahmetfurkankisacik@gmail.com>
2024-05-31 11:12:32 +0300

    21.05'ten-31.05.2024 tarihine kadar  olan dersler

--- src/day26_Maps/Maps02.java
+++ src/day26_Maps/Maps02.java
@@ -1,4 +1,9 @@
 package day26_Maps;
 
 public class Maps02 {
+    public static void main(String[] args) {
+        //Ornek 2:  Verilen Map'deki kisi isimlerinin character sayisi ile yaslarinin toplamini bulunuz.
+        //Java'da Map.Entry kullanarak bir Set üzerinde loop yapabilir ve
+        // hem anahtarları (getKey()) hem de değerleri (getValue()) elde edebilirsiniz.
+    }
 }
